459afe786e7b51ca450eaf6a1df3b5ea091f82d5
WonderingAlbatross <99506538+WonderingAlbatross@users.noreply.github.com>
2024-05-15 03:18:09 -0400

    Delete models directory

--- models/Player.py
+++
@@ -1,8 +0,0 @@
-from pokemon import Pokemon
-
-class Player:
-    def __init__(self, code, name):
-        self.code = code
-        self.name = name
-        self.pokemon = {}
-        self.side = {}
