1a96662c70f867ad9d26d8248896a8ead224a465
WonderingAlbatross <99506538+WonderingAlbatross@users.noreply.github.com>
2024-05-25 16:29:01 -0400

    Add files via upload

--- ANN_Classifier01.py
+++ ANN_Classifier01.py
@@ -8,6 +8,7 @@ from torch.utils.data import DataLoader, TensorDataset
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
 
 
 class ANN(nn.Module):
@@ -66,19 +67,13 @@ def get_Tensor(df_path):
     return X_tensor.to(device), y_tensor.to(device), weight_tensor.to(device)
 
 
-def start_learning(train_path,test_path,lr,l1_size,l2_size,epoch_num):
-    save_model_name = 'model'+'_'+str(l1_size)+'_'+str(l2_size)
-
-
-
+def start_learning(train_path,test_path,lr,l1_size,l2_size,batch_size,epoch_num):
 
     train_X, train_y, train_w = get_Tensor(train_path)
     test_X, test_y, test_w = get_Tensor(test_path)
     train_tensor = TensorDataset(train_X, train_y, train_w)
     input_size = test_X.shape[1]
-    dataloader = DataLoader(train_tensor, batch_size=1024, shuffle=True)
-
-
+    dataloader = DataLoader(train_tensor, batch_size, shuffle=True)
 
 
     if os.path.exists(save_model_name+'.pth'):
@@ -97,10 +92,8 @@ def start_learning(train_path,test_path,lr,l1_size,l2_size,epoch_num):
     if os.path.exists(save_model_name+'_loss.csv'):
         loss_df = pd.read_csv(save_model_name+'_loss.csv')
         starting_epoch += int(loss_df.iloc[-1]['epoch'])
-        print(loss_df.iloc[-1])
 
 
-    print('se',starting_epoch)
     for epoch in range(epoch_num):
         for inputs, labels, weights in dataloader:
             inputs, labels, weights = inputs, labels, weights
@@ -120,7 +113,6 @@ def start_learning(train_path,test_path,lr,l1_size,l2_size,epoch_num):
             output_test = model(test_X)
             loss_test = criterion(output_test, test_y)
             loss_test = (loss_test * test_w).sum()/test_w.sum()
-
             test_loss['epoch'].append(epoch+starting_epoch)
             test_loss['Test_Loss'].append(loss_test.item())
             print(f'Epoch {epoch+starting_epoch}, Test_Loss: {loss_test.item()}')
@@ -131,21 +123,38 @@ def start_learning(train_path,test_path,lr,l1_size,l2_size,epoch_num):
 
     if os.path.exists(save_model_name+'_loss.csv'):
         loss_df = pd.read_csv(save_model_name+'_loss.csv')
-        pd.concat([loss_df,new_loss_df], sort=False).to_csv(save_model_name+'_train.csv', index=False)
+        pd.concat([loss_df,new_loss_df], sort=False).to_csv(save_model_name+'_loss.csv', index=False)
     else:
         new_loss_df.to_csv(save_model_name+'_loss.csv', index=False)
     torch.save(model, save_model_name+'.pth')
 
+def plot_loss(loss_path):
+    df = pd.read_csv(loss_path)
+    plt.figure(figsize=(10, 5)) 
+    plt.scatter(df['epoch'], df['Loss'], label='Training Loss', color='blue', s = 2) 
+    plt.scatter(df['epoch'], df['Test_Loss'], label='Test Loss', color='red', s = 5) 
 
+    plt.ylim(0.4, 0.6)
+    plt.title('Training and Test Loss vs Epoch')
+    plt.ylabel('Loss')
+    plt.xticks([epoch for epoch in df['epoch'] if epoch % 100 == 0])
+    plt.legend()
+
+    plt.show()
 
 
 train_path = 'vectorized_train01.csv'
 test_path = 'vectorized_test01.csv'
-lr = 0.000001
+lr = 0.0001
 l1_size = 256
 l2_size = 128
+batch_size = 1024
 epoch_num = 100
+
+save_model_name = 'model'+'_'+str(l1_size)+'_'+str(l2_size)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("cuda ready?",torch.cuda.is_available())
 
-start_learning(train_path,test_path,lr,l1_size,l2_size,epoch_num)
+start_learning(train_path,test_path,lr,l1_size,l2_size,batch_size,epoch_num)
+
+plot_loss(save_model_name+'_loss.csv')
